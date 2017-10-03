6175a69e40140b1cb3bfc217797533291c5c3c2e
sadario <olafni@stud.ntnu.no>
2017-10-03 19:26:38 +0200

    For intern testing av workspace-oppsett, etc

---
+++ src/main/java/no/ntnu/imt3281/project1/Button.java
@@ -0,0 +1,15 @@
+package no.ntnu.imt3281.project1;
+
+public class Button extends BaseComponent {
+
+	public void setText(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
