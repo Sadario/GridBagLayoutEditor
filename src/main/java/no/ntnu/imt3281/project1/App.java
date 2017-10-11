8ad2daa78040251a22c9fef9aa09d947f7ef6e4f
Thomas <thomas.g@hotmail.no>
2017-10-11 20:37:55 +0200

    Started on I18N class. Started binding together all the loose pieces into one single application.

--- src/main/java/no/ntnu/imt3281/project1/App.java
+++ src/main/java/no/ntnu/imt3281/project1/App.java
@@ -1,18 +1,40 @@
 package no.ntnu.imt3281.project1;
 
-import javax.swing.JPanel;
+import javax.swing.JFrame;
 
-
-public class App extends JPanel
+/**
+ * App class serves as a container for the whole application and also contains
+ * the static main function.
+ * 
+ * @author thomasgg
+ * @author sadario
+ *
+ */
+public class App extends JFrame
 {
 	GBLEDataModel data;
 	
+	/**
+	 * 
+	 * 
+	 */
 	public App() {
+		super("Window title test");
 		data = new GBLEDataModel();
 	}
 	
+	/**
+	 * 
+	 * 
+	 * @param args
+	 */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App window = new App();
+        // linjene under kan og puttes i App CTOR (uten window.) - bedre?
+		window.setSize(800, 400);
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		window.setVisible(true);
+        
     }
 }
---
+++ src/main/java/no/ntnu/imt3281/project1/I18N.java
@@ -0,0 +1,48 @@
+package no.ntnu.imt3281.project1;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Har 2 forslag til impelmentasjon av denne:
+ * 
+ * Statisk klasse/funksjoner slik at alle eksisterende klasser
+ * og funksjoner kan kalle på denne klassens metoder direkte
+ *  eks: String columnHeaderType = I18N.getString("columnName.type");
+ * 
+ * Composition(?) med protected I18N objekt i App klassen (antar dette er øverste klasse) 
+ * der alle underklasser har tilgang til objektet og dens funksjoner.
+ * eks: I18N localLang = new I18N;
+ *      ...
+ *      localLang.getString("columnName.type");
+ * 
+ * oppdater public GBLEDataModel() når vi har kommet til en enighet
+ * 
+ * 
+ * @author thomasgg
+ * @author sadario
+ *
+ */
+public class I18N {
+	
+	private Locale locale;
+	private ResourceBundle bundle;
+	
+	/**
+	 * No argument constructor
+	 */
+	public I18N() {
+		locale = Locale.getDefault();
+		bundle = ResourceBundle.getBundle("no.ntnu.imt3281.project1.AppResourcesBundle", locale);
+	}
+	
+	/**
+	 * 
+	 * @param key String key with a match in the AppResourcesBundle .properties files.
+	 * @return String value belonging to the matching key argument
+	 */
+	public String getString(String key) {
+		return bundle.getString(key);
+	}
+
+}
