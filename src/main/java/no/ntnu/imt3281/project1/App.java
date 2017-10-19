4362a9338ec152441f811b7c19ac2236a396eb31
Thomas <thomas.g@hotmail.no>
2017-10-19 12:59:38 +0200

    minor updates

--- src/main/java/no/ntnu/imt3281/project1/App.java
+++ src/main/java/no/ntnu/imt3281/project1/App.java
@@ -730,31 +730,6 @@ public class App extends JFrame {
 		App window = new App();
     }
     
-	
-
-    
-    
-/*	
-	private class PopupListener implements PopupMenuListener {
-
-		@Override
-		public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
-			
-		}
-
-		@Override
-		public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
-			
-		}
-
-		@Override
-		public void popupMenuCanceled(PopupMenuEvent e) {
-			
-		}
-		
-	}
-*/
-
 }
 
 
